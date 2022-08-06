616773d77d8eaebc904550295b835c76e5c12b8d
Toyosi Taiwo <adekunletaiwo43@yahoo.com>
2022-08-06 20:51:42 +0100

    change introduction text

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -20,32 +20,16 @@ function Home2() {
             </h1>
             <p className="home-about-body">
               I fell in love with programming and I have at least learnt
-              something, I think… 🤷‍♂️
+              something, I think…. 🤷‍♂️
               <br />
               <br />I am fluent in classics like
               <i>
-                <b className="purple"> Javascript and Java. </b>
+                <b className="purple"> Javascript and Ruby. </b>
               </i>
               <br />
               <br />
-              My field of Interest's are building new &nbsp;
-              <i>
-                <b className="purple">Web Technologies and Products </b> 
-              </i>
-              <br />
-              <br />
-              Whenever possible, I also apply my passion for developing products
-              with <b className="purple">Node.js</b> and
-              <i>
-                <b className="purple">
-                  {" "}
-                  Modern Javascript Library and Frameworks
-                </b>
-              </i>
-              &nbsp; like
-              <i>
-                <b className="purple"> React.js</b>
-              </i>
+              I am passionate about learning new technologies as well as building things.
+              
             </p>
           </Col>
           <Col md={4} className="myAvtar">
