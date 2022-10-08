eab91b8b34d607170235b6fe41b99049f788edc3
Toyosi Taiwo <adekunletaiwo43@yahoo.com>
2022-10-08 21:38:05 +0100

    Update social link

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -69,7 +69,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.linkedin.com/in/toyosi-taiwo-93b9a7110/"
+                  href="https://www.linkedin.com/in/taiwo-toyosi/"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
