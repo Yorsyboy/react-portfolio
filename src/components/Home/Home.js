d045846824f000f799df344d32e667ffb9cb1072
Toyosi Taiwo <adekunletaiwo43@yahoo.com>
2022-10-25 22:40:05 +0100

    Refactor home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,42 +1,92 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import homeLogo from "../../Assets/home-main.svg";
-import Particle from "../Particle";
-import Home2 from "./Home2";
-import Type from "./Type";
+import myImg from "../../Assets/myimg.JPG";
+import { useHistory } from "react-router-dom";
+import Tilt from "react-parallax-tilt";
+import {
+  AiFillGithub,
+  AiOutlineTwitter,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
 
 function Home() {
+  const navigate = useHistory();
   return (
-    <section>
-      <Container fluid className="home-section resume-section" id="home">
-        <Particle />
-        <Container className="home-content">
-          <Row>
-            <Col md={7} className="home-header">
-              <h1 style={{ paddingBottom: 15 }} className="heading">
-                Hi <span className="wave" role="img" aria-labelledby="wave">👋🏻</span>
-              </h1>
+    <Container fluid className="home-about-section" id="about">
+      <Container>
+        <Row>
+          <Col md={8} className="home-about-description">
+            <h1 style={{ paddingBottom: 15 }} className="heading">
+              Hi <span className="wave" role="img" aria-labelledby="wave">👋🏻</span>
+            </h1>
 
-              <h1 className="heading-name">
-                I'm
-                <strong className="main-name"> Taiwo Toyosi</strong>
-              </h1>
+            <h1 className="heading-name">
+              I'm
+              <strong className="main-name"> Taiwo Toyosi</strong>
+            </h1>
 
-              <div style={{ padding: 50, textAlign: "left" }}>
-                <Type />
-              </div>
-            </Col>
-
-            <Col md={5} style={{ paddingBottom: 20 }}>
-              <img src={homeLogo} alt="home pic" className="img-fluid" />
-            </Col>
-          </Row>
-        
-        </Container>
+            {/* <h1 style={{ fontSize: "2.6em" }}>
+              LET ME <span className="purple"> INTRODUCE </span> MYSELF
+            </h1> */}
+            <p className="home-about-body">
+              A Software Developer passionate about building interactive interfaces,
+              I can help you build a product, feature or website. My language stack includes Ruby,
+              Ruby on Rails, JavaScript and React.I am passionate about learning new technologies
+              as well as building things.I fell in love with programming ever since I have been building
+              projects with the stacks mentioned above.
+              I am a self-taught developer and I am currently looking for new opportunities to grow within a 
+              technical team.
+            </p>
+            <button className="btn btn-primary" onClick={() => navigate.push('/project')}>See Projects</button>
+          </Col>
+          <Col md={4} className="myAvtar">
+            <Tilt>
+              <img src={myImg} className="img-fluid" alt="avatar" />
+            </Tilt>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12} className="home-about-social">
+            <h1>FIND ME ON</h1>
+            <p>
+              Feel free to <span className="purple">connect </span>with me
+            </p>
+            <ul className="home-about-social-links">
+              <li className="social-icons">
+                <a
+                  href="https://github.com/Yorsyboy"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <AiFillGithub />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://twitter.com/Tboytaiwo"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <AiOutlineTwitter />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://www.linkedin.com/in/taiwo-toyosi/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <FaLinkedinIn />
+                </a>
+              </li>
+            </ul>
+          </Col>
+        </Row>
       </Container>
-      <Home2 />
-    </section>
+    </Container>
   );
 }
-
 export default Home;
