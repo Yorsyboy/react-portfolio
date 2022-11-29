7a6e276cd73729715cddc9c1c74e10ce78e49ddc
Toyosi Taiwo <adekunletaiwo43@yahoo.com>
2022-11-29 22:17:01 +0100

    Required changes

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -18,39 +18,30 @@ function Techstack() {
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
         <DiHtml5 />
-        <p>HTML5</p>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiCss3 />
-        CSS
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiTailwindcss />
-        Tailwind
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiRuby />
-        Ruby
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiJavascript1 />
-        Javascript
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiNodejs />
-        Node
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiReact />
-        React
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiPostgresql />
-        Postgresql
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiGit />
-        <p>Git</p>
       </Col>
     </Row>
   );
